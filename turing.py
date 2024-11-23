f2057ae2ec1c0d7b11aa66ac92d6e1ccf9f52b71
melissaGit124 <83406039+melissaGit124@users.noreply.github.com>
2024-11-22 21:46:47 -0300

    Update turing.py

--- turing.py
+++ turing.py
@@ -1,36 +1,31 @@
 def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
+    mensagem = "Alan Mathison Turing foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
     return mensagem
-
 
 def doutorado():
-    mensagem = ""
+    mensagem = "Alan Turing concluiu seu doutorado na Universidade de Princeton em 1938, onde trabalhou sob a orientação de Alonzo Church. Durante seu doutorado, ele aprofundou sua pesquisa sobre lógica matemática e teoria da computação."
     return mensagem
-
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Turing fez diversas contribuições notáveis, incluindo o conceito da 'Máquina de Turing', essencial para a teoria da computação. Ele também ajudou a decifrar os códigos alemães Enigma durante a Segunda Guerra Mundial, trabalho que foi fundamental para a vitória dos Aliados."
     return mensagem
-
 
 def artigos():
-    mensagem = ""
+    mensagem = "Alguns dos principais artigos de Turing incluem:\n- 'On Computable Numbers, with an Application to the Entscheidungsproblem' (1936)\n- 'Systems of Logic Based on Ordinals' (1938)\n- 'Computing Machinery and Intelligence' (1950), onde ele propõe o famoso 'Teste de Turing'."
     return mensagem
-
 
 def citacoes():
-    mensagem = ""
+    mensagem = "Citações de Alan Turing:\n- 'Uma máquina digital pode, se adequadamente programada, simular o comportamento de qualquer outra máquina digital.'\n- 'Podemos ver apenas um curto trecho do futuro, mas podemos ver o suficiente para perceber que há muito a ser feito.'"
     return mensagem
-
 
 def sair():
     mensagem = "\nObrigado pela leitura!"
     return mensagem
 
-
 def erro():
     mensagem = "\nOpção inválida!"
     return mensagem
+
 
 
 print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
